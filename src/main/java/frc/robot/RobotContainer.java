1e9684387a2828f7279bc23821ff66896f20c24b
imlivinginmyhouse <iicastiiwasnottaken@gmail.com>
2025-11-10 16:56:07 -0800

    subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,8 +4,10 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.Autos;
 import frc.robot.commands.Drive;
 import frc.robot.commands.LiftArm;
@@ -22,7 +24,8 @@ public class RobotContainer {
   private final Arm arm = new Arm();
   private final CommandXboxController controller = new CommandXboxController(0);
   private final Drive drive = new Drive(drivetrain, controller);
-  private Trigger lift_arm_button = 
+  private Trigger lift_arm_button_up = controller.a();
+  private Trigger lift_arm_button_down = controller.b();
 
   public RobotContainer() {
     configureBindings();
@@ -31,7 +34,8 @@ public class RobotContainer {
   }
 
   public void configureBindings() {
-
+    lift_arm_button_up.whileTrue(new LiftArm(arm, 0.5));
+    lift_arm_button_down.whileFalse(new LiftArm(arm, -0.5));
   }
 
   public Command getAutonomousCommand() {
