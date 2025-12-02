a45133c941785e0b59230a4d7039df685476fd96
severtee <severtee@severtees-MacBook-Pro.local>
2025-12-01 16:30:14 -0800

    Final touches to the Falcon Arm

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,8 +11,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.BasicAuto;
 import frc.robot.commands.Drive;
 import frc.robot.commands.GotoPos;
-import frc.robot.commands.LiftArm;
-import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drivetrain;
 import frc.robot.subsystems.FalconArm;
 /**
